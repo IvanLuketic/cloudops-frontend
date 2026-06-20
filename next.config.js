3f47420603cc3cf92b20950a2eb85af37c1d83f7
IvanLuketic <luketicivan002@gmail.com>
2026-06-20 18:27:59 +0200

    Fix: Remove type:module from package.json, use CommonJS

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: 'http://3.74.42.88:8081',
   },
-  generateBuildId: async () => {
-    return 'stable-build-' + Date.now()
-  },
 }
 
 module.exports = nextConfig
