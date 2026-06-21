f66e7bca210c342f3ac521e476c784be0745a960
IvanLuketic <luketicivan002@gmail.com>
2026-06-21 15:01:15 +0200

    Fix: Add API URL to port 8080

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: 'http://3.74.42.88:8081',
+    NEXT_PUBLIC_API_URL: 'http://54.171.1.214:8080',
   },
 }
-
-module.exports = nextConfig
